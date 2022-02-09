e895118894fc27fe86bf982c416091d7a9c2ab7f
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2022-02-09 13:13:05 -0300

    Add navbar to index

--- simplyrecipes/src/pages/index.js
+++ simplyrecipes/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Navigation from "../components/navigation/navigation";
 
 export default function Home() {
   return (
       <div>
+          <Navigation />
         <h1>Hello world!</h1>
       </div>
   )
