2000154ba194467f75132fd58249cf4fb235e813
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2022-02-13 12:11:58 -0300

    Add recipes, tags building blocks

---
+++ simplyrecipes/src/pages/recipes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layouts'
+
+export default function Recipes(){
+    return (
+        <Layout>
+            <h1>This is the recipes page</h1>
+        </Layout>
+    )
+}
